4dcda7592756fccd8e318c363b4f9d41e2fecd44
Joel Weiss <joel.weiss@hpe.com>
2023-12-07 14:21:34 +0100

    merged everything together for Trulens

---
+++ backend/helper_functions/set_env_variables.py
@@ -0,0 +1,17 @@
+import os
+
+def load_env_variables():
+    os.environ['PROJECT_NUMBER'] = '993668300869'
+    os.environ['TestVariable'] = 'Secret loading works'
+    os.environ['ENDPOINT_ID'] = '3910824321534132224'
+    os.environ['LOCATION'] = 'us-central1'
+    os.environ['CREDENTIALS_JSON'] = load_credential_file("/Users/hpe/Programming/SimplifAI/GUI/igneous-visitor-407107-17758a215e3e.json")
+    os.environ["HUGGINGFACEHUB_API_TOKEN"] = "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"
+    os.environ["OPENAI_API_KEY"] = ''
+
+def load_credential_file(filenmae):
+
+    with open(filenmae) as f:
+        data = f.read()
+
+    return data
