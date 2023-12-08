f26df44e198eff6e4ff0f18abf17ff8c444ac941
Joel Weiss <joel.weiss@hpe.com>
2023-12-08 09:59:05 +0100

    small change in env variables

--- helper_functions/set_env_variables.py
+++ helper_functions/set_env_variables.py
@@ -5,7 +5,7 @@ def load_env_variables():
     os.environ['TestVariable'] = 'Secret loading works'
     os.environ['ENDPOINT_ID'] = '3910824321534132224'
     os.environ['LOCATION'] = 'us-central1'
-    os.environ['CREDENTIALS_JSON'] = load_credential_file("/Users/hpe/Programming/SimplifAI/GUI/igneous-visitor-407107-17758a215e3e.json")
+    os.environ['CREDENTIALS_JSON'] = load_credential_file("/Users/hpe/Programming/SimplifAI/GUI-Tests/igneous-visitor-407107-17758a215e3e.json")
     os.environ["HUGGINGFACEHUB_API_TOKEN"] = "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"
     os.environ["OPENAI_API_KEY"] = ''
 
