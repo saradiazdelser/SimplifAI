85c7a88a216b28849c8a7922255af8f80abee217
Sara Díaz <sara.diaz.delser@gmail.com>
2024-03-01 11:07:12 +0100

    add eval prompts

--- eval_prompts.py
+++ eval_prompts.py
@@ -1,21 +1,15 @@
-EVAL_PROMPT_MIXTRAL = {
-    "prompt_text": """[INST] Assign a score (0-1 points) to the translation of the following text from English to Simple English.
-Simple English is a version of the English language that aims to present information in a way that is easy to understand for individuals with limited proficiency in English or those who are learning the language. 
-Texts in Simple English must follow these 10 requirements. Assing 1 point per requirement met.
-1. Each sentence expresses only one idea.
-2. The subject of each sentence must be explicit. Do not use pronouns as subjects.
-3. Use short sentences and simple vocabulary. Use simple and direct language.
-4. Do not use negations.
-5. Do not use too many numbers or mathematical denominations. If there is no choice but to insert a number, always use digits.
-6. Do not use confusing metaphors or complex comparisons.
-7. Be clear, concise and direct.
-8. Do not use technical terms, abbreviations and initials.
-9. Text should be clear and coherent.
-10. Avoid unnecessary ideas, words, sentences or phrases.
-Original text (English):
-{input_text}
-Rewritten text (Simple English):
-{ouput_text}
-Score: [/INST] """,
-    "variables": ["input_text", "output_text"],
+REQUIREMENT_4 = {
+    "prompt_text": """"[INST] Assign a score (0-10 points) to the translation of the following text from English to Simple English.
+    One requirement of the Simple English is to ''Do not use negations''.
+    Below, there are some examples of Simple English text and the output will be 10 if it meets the requirement of ''Do not use negations'' in every sentence, and will be 0 if it does not meet it in any sentence. 
+    Text 1: "The capybara is a big rodent.\nThe capybara lives in South America.\nThe capybara is the biggest rodent alive.\n It lives in South America.\nCapybaras are part of the Hydrochoerus family.\n The capybara lives in savannas and rainforests.\nThe capybara lives near places with water.\n" 
+    The assigned score to Text 1 is (0-10): 10 points
+    Text 2: "The capybara is not a big rodent.\nThe capybara does not live in South America.\nThe capybara is not the biggest rodent alive.\n It does not live in South America.\nCapybaras are not part of the Hydrochoerus family.\n The capybara does not live in savannas and rainforests.\nThe capybara does not live near places with water.\n" 
+    The assigned score to Text 2 is (0-10): 0 points
+    Text 3: "The capybara is a big rodent.\nThe capybara does live in South America.\nThe capybara is the biggest rodent alive.\n It does not live in South America.\nCapybaras are not part of the Hydrochoerus family.\n The capybara does not live in savannas and rainforests.\n" 
+    The assigned score to Text 3 is (0-10): 5 points
+    Text 4: "{text}"
+    The assigned score to Text 4 is (0-10): [/INST] 
+""",
+    "variables": ["text"],
 }
