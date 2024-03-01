1bf121e9945f7264d17b089eb634e84aa510d27c
Claudia Garmendia Fierro <claudia.garmendia@hpe.com>
2024-03-01 14:37:42 +0100

    final_prompts

--- eval_prompts.py
+++ eval_prompts.py
@@ -1,52 +1,72 @@
 REQUIREMENT_4 = {
-    "prompt_text": """"[INST] Assign a score (0-10 points) to the translation of the following text from English to Simple English.
-    One requirement of the Simple English is to ''Do not use negations''.
-    Below, there are some examples of Simple English text and the output will be 10 if it meets the requirement of ''Do not use negations'' in every sentence, and will be 0 if it does not meet it in any sentence. 
-    Text 1: "The capybara is a big rodent.\nThe capybara lives in South America.\nThe capybara is the biggest rodent alive.\nCapybaras are part of the Hydrochoerus family.\n The capybara lives in savannas and rainforests.\nThe capybara lives near places with water.\n" 
+    "prompt_text": """"<s>[INST] Assign a score to the given text. The output must be a number 0, 5, 10.
+    The output will be '10' if it meets the requirement of ''Do not use negations'' in every sentence, and will be '0' if it does not meet it in any sentence. If some sentences meet the requirement, but other not meet it, set a score of '5'.
+
+    Text 1: "The capybara is a big rodent.\nThe capybara lives in South America.\nThe capybara is the biggest rodent alive.\n It lives in South America.\nCapybaras are part of the Hydrochoerus family.\n The capybara lives in savannas and rainforests.\nThe capybara lives near places with water.\n" 
     The assigned score to Text 1 is (0-10): 10 points
+
     Text 2: "The capybara is not a big rodent.\nThe capybara does not live in South America.\nThe capybara is not the biggest rodent alive.\n It does not live in South America.\nCapybaras are not part of the Hydrochoerus family.\n The capybara does not live in savannas and rainforests.\nThe capybara does not live near places with water.\n" 
     The assigned score to Text 2 is (0-10): 0 points
-    Text 3: "The capybara is a big rodent.\nThe capybara does live in South America.\nThe capybara is the biggest rodent alive.\n It does not live in South America.\nCapybaras are not part of the Hydrochoerus family.\n The capybara does not live in savannas and rainforests.\n" 
+
+    Text 3: "The capybara is not a big rodent.\nThe capybara does live in South America.\nThe capybara is the biggest rodent alive.\n It does not live in South America.\nCapybaras are not part of the Hydrochoerus family.\n The capybara does not live in savannas and rainforests.\n" 
     The assigned score to Text 3 is (0-10): 5 points
+    [/INST]</s> 
+
+    [INST] 
     Text 4: "{text}"
-    The assigned score to Text 4 is (0-10): [/INST] 
+    The assigned score to Text 4 is (0-10): 
+
+    The output must be a number without explanations. [/INST]
 """,
     "variables": ["text"],
 }
 
 
 REQUIREMENT_2 = {
-    "prompt_text": """[INST] Assign a score (0-10 points) to the input text.
-    One requirement of the Simple English is to ''The subject of each sentence must be explicit''.
+    "prompt_text": """<s>[INST] Assign a score to the given text. The requirement of each sentence is 'The subject of each sentence must be explicit'.
 
-    Below, there are some examples of Simple English text and the output will be 10 if it meets the requirement of ''The subject of each sentence must be explicit'' in every sentence, and will be 0 if it does not meet it in any sentence. 
+    The output will be '10' if it meets the requirement in every sentence, will be '0' if it does not meet it in any sentence or will be '5' if some sentences meet the requirement, but other not meet it.
+
     Text 1: "The capybara is a big rodent.\nThe capybara lives in South America.\nThe capybara is the biggest rodent alive.\n Capybaras are part of the Hydrochoerus family.\n The capybara lives in savannas and rainforests.\nThe capybara lives near places with water.\n" 
     The assigned score to Text 1 is (0-10): 10 points
-    Text 2: "The capybara is not a big rodent.\nIt does not live in South America.\nIt is not the biggest rodent alive.\n It does not live in South America.\nThey are not part of the Hydrochoerus family.\n It does not live in savannas and rainforests.\nIt does not live near places with water.\n" 
-    The assigned score to Text 2 is (0-10): 0 points
-    Text 3: "The capybara is a big rodent.\nIt does live in South America.\nIt is the biggest rodent alive.\n It does not live in South America.\nCapybaras are not part of the Hydrochoerus family.\n The capybara does not live in savannas and rainforests.\n" 
-    The assigned score to Text 3 is (0-10): 5 points
     
-	Text 4: "{text}"
-    The assigned score to Text 4 is (0-10): [/INST] 
+    Text 2: "It is a big rodent.\nIt lives in South America.\nIt is the biggest rodent alive.\n They are part of the Hydrochoerus family.\n It lives in savannas and rainforests.\nIt lives near places with water.\n" 
+    The assigned score to Text 2 is (0-10): 0 points
+    
+    Text 3: "The capybara is a big rodent.\nIt lives in South America.\nThe capybara is the biggest rodent alive.\n They are part of the Hydrochoerus family.\n It lives in savannas and rainforests.\nThe capybara lives near places with water.\n" 
+    The assigned score to Text 3 is (0-10): 5 points
+    [/INST]</s> 
+
+    [INST] 
+    Text 4: "{text}"
+    The assigned score to Text 4 is (0-10): 
+
+    The output must be a number without explanations. [/INST]
 """,
     "variables": ["text"],
 }
 
 
 REQUIREMENT_5 = {
-    "prompt_text": """[INST] Assign a score (0-10 points) to the input text.
-    One requirement of the Simple English is to ''Do not use too many numbers''.
+    "prompt_text": """<s>[INST] Assign a score to the given text. The requirement of each sentence is 'Do not use too many numbers'.
 
-    Below, there are some examples of Simple English text and the output will be 10 if it meets the requirement of ''Do not use too many numbers'' in every sentence, and will be 0 if it does not meet it in any sentence. 
+    The output will be '10' if it meets the requirement in every sentence, will be '0' if it does not meet it in any sentence or will be '5' if some sentences meet the requirement, but other not meet it.
 
     Text 1: "The capybara is a big rodent.\nThe capybara lives in South America.\nThe capybara is the biggest rodent alive.\n Capybaras are part of the Hydrochoerus family.\n The capybara lives in savannas and rainforests.\nThe capybara lives near places with water.\n" 
     The assigned score to Text 1 is (0-10): 10 points
-    Text 2: "There are 1,200,000 capybaras in Brazil.\nThe capybara measure three to four feet in length and one to two feet at shoulder height.\n An adult capybara weighs 60 to 174 pounds, depending on the sex.\n" 
+    
+    Text 2: "There are 1,200,000 capybaras in Brazil.\nThe capybara measure from 3 to 4 feet in length and 1 to 2 feet at shoulder height.\n An adult capybara weighs 60 to 174 pounds, depending on the sex.\n" 
     The assigned score to Text 2 is (0-10): 0 points
     
-	Text 3: "{text}"
-    The assigned score to Text 3 is (0-10): [/INST] 
+    Text 3: "The capybara is a big rodent.\nAn adult capybara weighs 60 to 174 pounds, depending on the sex.\n" 
+    The assigned score to Text 3 is (0-10): 5 points
+    [/INST]</s> 
+
+    [INST] 
+    Text 4: "{text}"
+    The assigned score to Text 4 is (0-10): 
+
+    The output must be a number without explanations. [/INST] 
 """,
     "variables": ["text"],
 }
